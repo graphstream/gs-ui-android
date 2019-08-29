7c01876c6cc8fc5c311f11c5813949285b71e2ee
hichbra <hicham.brahimi@laposte.net>
2019-08-29 16:01:30 +0200

    add mouvement

--- app/src/main/java/org/graphstream/ui/android_viewer/util/DefaultMouseManager.java
+++ app/src/main/java/org/graphstream/ui/android_viewer/util/DefaultMouseManager.java
@@ -6,6 +6,7 @@ import android.view.MotionEvent;
 import android.view.ScaleGestureDetector;
 import android.view.SurfaceView;
 
+import org.graphstream.ui.geom.Point3;
 import org.graphstream.ui.graphicGraph.GraphicElement;
 import org.graphstream.ui.graphicGraph.GraphicGraph;
 import org.graphstream.ui.view.View;
@@ -31,7 +32,7 @@ public class DefaultMouseManager implements MouseManager, android.view.View.OnTo
 
     protected GraphicElement curElement;
 
-    protected float x1, y1;
+    protected float x1, x2, y1, y2;
 
     /**
      * Manager to detect gesture (pinch)
@@ -72,56 +73,112 @@ public class DefaultMouseManager implements MouseManager, android.view.View.OnTo
     @Override
     public boolean onTouch(android.view.View v, MotionEvent event) {
 
+        /** Handle the "pinch" gesture **/
         gestureManager.onTouchEvent(event);
 
-        switch(event.getActionMasked())
-        {
-            case MotionEvent.ACTION_DOWN:
-                curElement = view.findGraphicElementAt(types,event.getX(), event.getY());
+        int pointerCount = event.getPointerCount();
 
-                if (curElement != null) {
-                    mouseButtonPressOnElement(curElement, event);
-                } else {
-                    x1 = event.getX();
-                    y1 = event.getY();
-                    mouseButtonPress(event);
-                    view.beginSelectionAt(x1, y1);
-                }
-                break;
-            case MotionEvent.ACTION_MOVE:
-                if (curElement != null) {
-                    elementMoving(curElement, event);
-                } else {
-                    view.selectionGrowsAt(event.getX(), event.getY());
-                }
-                break;
-            case MotionEvent.ACTION_UP:
-                if (curElement != null) {
-                    mouseButtonReleaseOffElement(curElement, event);
-                    curElement = null;
-                } else {
-                    float x2 = event.getX();
-                    float y2 = event.getY();
-                    float t;
+        /** One finger touch : "Selections" actions **/
+        if (pointerCount == 1) {
+            switch(event.getActionMasked()) {
+                case MotionEvent.ACTION_DOWN:
+                    curElement = view.findGraphicElementAt(types, event.getX(), event.getY());
 
-                    if (x1 > x2) {
-                        t = x1;
-                        x1 = x2;
-                        x2 = t;
+                    if (curElement != null) {
+                        mouseButtonPressOnElement(curElement, event);
+                    } else {
+                        x1 = event.getX();
+                        y1 = event.getY();
+                        mouseButtonPress(event);
+                        view.beginSelectionAt(x1, y1);
                     }
-                    if (y1 > y2) {
-                        t = y1;
-                        y1 = y2;
-                        y2 = t;
+                    break;
+                case MotionEvent.ACTION_MOVE:
+                    if (curElement != null) {
+                        elementMoving(curElement, event);
+                    } else {
+                        view.selectionGrowsAt(event.getX(), event.getY());
+                    }
+                    break;
+                case MotionEvent.ACTION_UP:
+                    if (curElement != null) {
+                        mouseButtonReleaseOffElement(curElement, event);
+                        curElement = null;
+                    } else {
+                        float x2 = event.getX();
+                        float y2 = event.getY();
+                        float t;
+
+                        if (x1 > x2) {
+                            t = x1;
+                            x1 = x2;
+                            x2 = t;
+                        }
+                        if (y1 > y2) {
+                            t = y1;
+                            y1 = y2;
+                            y2 = t;
+                        }
+
+                        mouseButtonRelease(event, view.allGraphicElementsIn(types, x1, y1, x2, y2));
+                        view.endSelectionAt(x2, y2);
+                    }
+                    break;
+                default:
+                    break;
+            }
+        } else { /** Two or more finger touch : "Moves" actions **/
+            Camera camera = view.getCamera();
+
+            switch(event.getActionMasked()) {
+                case MotionEvent.ACTION_DOWN:
+                    x1 = event.getX(0); x2 = event.getX(1);
+                    y1 = event.getY(0); y2 = event.getX(1);
+                    /*float[] initPositionXFinger = new float[pointerCount];
+                    float[] initPositionYFinger = new float[pointerCount];
+
+                    for (int i = 0 ; i < pointerCount ; i++){
+                        initPositionXFinger[i] = event.getX(i);
+                        initPositionYFinger[i] = event.getY(i);
+                    }*/
+
+
+                    break;
+                case MotionEvent.ACTION_MOVE:
+                    double delta = camera.getGraphDimension() * 0.01f;
+                    delta *= camera.getViewPercent();
+                    Point3 p = camera.getViewCenter();
+
+
+                    // To the right
+                    if (event.getX(0) > x1 && event.getX(1) > x2){
+                        camera.setViewCenter(p.x + delta, p.y, 0);
+                    }
+                    // To the left
+                    else if (event.getX(0) < x1 && event.getX(1) < x2){
+                        camera.setViewCenter(p.x - delta, p.y, 0);
+                    }
+                    
+                    // Downwards
+                    if (event.getY(0) > x1 && event.getY(1) > x2){
+                        camera.setViewCenter(p.x, p.y - delta, 0);
+                    }
+                    // Upwards
+                    else if (event.getY(0) < x1 && event.getY(1) < x2) {
+                        camera.setViewCenter(p.x, p.y + delta, 0);
                     }
 
-                    mouseButtonRelease(event, view.allGraphicElementsIn(types,x1, y1, x2, y2));
-                    view.endSelectionAt(x2, y2);
-                }
-                break;
-            default:
-                break;
+                    break;
+                case MotionEvent.ACTION_UP:
+
+
+                    break;
+                default:
+                    break;
+            }
         }
+
+
         return true;
     }
 
@@ -177,7 +234,7 @@ public class DefaultMouseManager implements MouseManager, android.view.View.OnTo
             Camera camera = view.getCamera();
             float factor = detector.getScaleFactor() ;
 
-            camera.setViewPercent(Math.max(0.0001f, camera.getViewPercent() / factor));
+            camera.setViewPercent(Math.max(0.0001f, camera.getViewPercent() * factor));
             return true;
         }
     }
